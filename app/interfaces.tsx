890023b46954c15c089a220ea6bb349acb676ec8
effervescent000 <kislio01@gmail.com>
2023-03-14 14:40:03 -0400

    set up context to prep for storing team

--- app/interfaces.tsx
+++ app/interfaces.tsx
@@ -7,3 +7,39 @@ export interface PokeAPIResponse {
   count?: number;
   results?: any[];
 }
+
+interface IName {
+  name: string;
+  url: string;
+}
+
+interface IAbility {
+  ability: {
+    name: string;
+  };
+}
+
+interface IMove {
+  move: IName;
+  version_group_details: {
+    level_learned_at: number;
+    move_learn_method: IName;
+    version_group: IName;
+  }[];
+}
+
+interface IStat {
+  base_stat: number;
+  effort: number;
+  stat: IName;
+}
+
+export interface IPokemon {
+  abilities: IAbility[];
+  id: number;
+  moves: IMove;
+  name: string;
+  order: number;
+  stats: IStat[];
+  types: { slot: number; type: IName }[];
+}
