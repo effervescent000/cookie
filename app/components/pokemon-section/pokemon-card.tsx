06708f1c90858f20d6ee6024c959cfe63c1a726a
effervescent000 <kislio01@gmail.com>
2023-03-14 18:45:24 -0400

    select pokemon

--- app/components/pokemon-section/pokemon-card.tsx
+++ app/components/pokemon-section/pokemon-card.tsx
@@ -1,17 +1,35 @@
+import { useContext, useState } from "react";
 import type { IOptions } from "~/interfaces";
+import { PokemonContext } from "~/pokemon-context";
+import PokeAPIService from "~/utils/pokeapi-service";
 import Select from "../common/select";
 
-const PokemonInput = ({
-  pokemonOptions,
-  id,
-}: {
-  pokemonOptions: IOptions[];
-  id: number;
-}) => {
+const PokemonInput = ({ pokemonOptions }: { pokemonOptions: IOptions[] }) => {
+  const [selectedPokemon, setSelectedPokemon] = useState("");
+
+  const { mergeIntoBench, idCounter } = useContext(PokemonContext);
+
+  const selectTarget = (target: string) => {
+    if (target && target !== selectedPokemon) {
+      const P = new PokeAPIService();
+      P.getPokemonByName(target)
+        .then((pokemon) => {
+          mergeIntoBench({ ...pokemon, id: idCounter });
+          setSelectedPokemon(target);
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div>
       <div>
-        <Select options={pokemonOptions} placeholder="Species" />
+        <Select
+          options={pokemonOptions}
+          placeholder="Species"
+          callback={selectTarget}
+          selection={selectedPokemon}
+        />
       </div>
     </div>
   );
