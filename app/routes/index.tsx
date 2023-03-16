08b744929bd2e9d7fa7be0fc883b062d099acf07
effervescent000 <kislio01@gmail.com>
2023-03-16 10:41:33 -0400

    rename file

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 import FilterSection from "~/components/filter-section/filter-section";
 import PokemonSection from "~/components/pokemon-section/pokemon-card-wrapper";
-import GenPicker from "~/components/gen-picker/gen-picker-main";
+import GenPicker from "~/components/gen-picker/gen-picker-wrapper";
 
 export default function Index() {
   return (
