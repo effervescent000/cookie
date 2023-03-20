59ac71055c0872fd1953d38dda93169bd4783b89
effervescent000 <kislio01@gmail.com>
2023-03-19 20:02:24 -0400

    move defensive calcs into helper func

---
+++ app/utils/helpers.ts
@@ -0,0 +1,26 @@
+import { DAMAGE_RELATION_VALUES } from "~/constants/types-constants";
+import type { IPokemonFull } from "~/interfaces";
+import type PokeAPIService from "./pokeapi-service";
+
+export const makeDefensiveValues = async (
+  pokemon: IPokemonFull,
+  P: PokeAPIService
+) => {
+  const thisPokeValues: { [key: string]: number } = {};
+  for (const typeObj of pokemon.types) {
+    const typeName = typeObj.type.name;
+    const typeResponse = await P.getType(typeName);
+    Object.entries(typeResponse.damage_relations).forEach(
+      ([damage_level, relatedTypes]) => {
+        if (damage_level.includes("_from")) {
+          relatedTypes.forEach((relatedType) => {
+            thisPokeValues[relatedType.name] =
+              (thisPokeValues[relatedType.name] || 0) +
+              DAMAGE_RELATION_VALUES[damage_level];
+          });
+        }
+      }
+    );
+  }
+  return thisPokeValues;
+};
