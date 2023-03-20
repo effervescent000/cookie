6bb92e4f3a5a10a24f2cb8d35cf80bfecbeb50f9
effervescent000 <kislio01@gmail.com>
2023-03-20 18:42:56 -0400

    add querying

--- app/components/filter-section/pokemon-mini-card.tsx
+++ app/components/filter-section/pokemon-mini-card.tsx
@@ -1,21 +1,31 @@
 import { useContext } from "react";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faCircleUp,
+  faDownload,
+} from "@fortawesome/free-solid-svg-icons";
 
 import type { IPokemonFull, IResourceListItem } from "~/interfaces";
 
 import { PokemonContext } from "~/pokemon-context";
-import PokeAPIService from "~/utils/pokeapi-service";
-import Icon from "../common/icon";
 import { properCase } from "~/utils/text-utils";
+
+import Icon from "../common/icon";
+import PokeAPIService from "~/utils/pokeapi-service";
+import { isFullPokemon } from "~/utils/type-guards";
 
 const PokemonMiniCard = ({
   full = false,
   poke,
-  hideMergeIcon,
+  api,
+  hideIcons,
+  merge,
 }: {
   full?: boolean;
   poke: IPokemonFull | IResourceListItem;
-  hideMergeIcon?: boolean;
+  hideIcons?: boolean;
+  api: PokeAPIService;
+  merge: (target: IPokemonFull) => void;
 }) => {
   const { mergeIntoBench, idCounter } = useContext(PokemonContext);
 
@@ -27,12 +37,23 @@ const PokemonMiniCard = ({
     });
   };
 
+  const queryAndAddPokemon = async (target: string) => {
+    const fullPokemon = await api.getPokemonByName([target]);
+    merge(fullPokemon[0]);
+  };
+
   return (
     <div className="flex w-full justify-between">
       <div>{properCase(poke.name)}</div>
-      {!hideMergeIcon && (
-        <div>
-          <Icon icon={faCheck} onClick={selectPokemon} />
+      {!hideIcons && (
+        <div className="grid grid-cols-2 gap-1">
+          <Icon icon={faCircleUp} onClick={selectPokemon} />
+          {!isFullPokemon(poke) && (
+            <Icon
+              icon={faDownload}
+              onClick={() => queryAndAddPokemon(poke.name)}
+            />
+          )}
         </div>
       )}
     </div>
