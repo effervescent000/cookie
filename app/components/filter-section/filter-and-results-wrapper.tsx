c682ddb48f6cc65350dcb49606d8017d176c6251
effervescent000 <kislio01@gmail.com>
2023-03-20 23:24:36 -0400

    filter results by game in a very jank way

--- app/components/filter-section/filter-and-results-wrapper.tsx
+++ app/components/filter-section/filter-and-results-wrapper.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 
 import type { IPokemonFull, IResourceListItem } from "~/interfaces";
+import { PokemonContext } from "~/pokemon-context";
 
 import PokeAPIService from "~/utils/pokeapi-service";
+import { isFullPokemon } from "~/utils/type-guards";
 
 import FilterWrapper from "./filters-wrapper";
 import ResultsWrapper from "./results-wrapper";
@@ -17,6 +19,7 @@ const FilterSection = ({
   merge: (target: IPokemonFull) => void;
 }) => {
   const [filters, setFilters] = useState({ name: "", type1: "", type2: "" });
+  const { versionGroup } = useContext(PokemonContext);
 
   const mergeFilters = (newFilter: { [key: string]: string }) =>
     setFilters({ ...filters, ...newFilter });
@@ -24,10 +27,26 @@ const FilterSection = ({
   useEffect(() => {
     const fetchPokemon = async () => {
       const P = new PokeAPIService();
-      setAllPokemon(await P.getAllPokemon());
+      const pokemonResult = await P.getAllPokemon();
+      const versions = (await P.getVersionGroup(versionGroup)).versions.map(
+        ({ name }) => name
+      );
+      const queryResults = await Promise.all(
+        pokemonResult.map(async (poke) => {
+          if (!isFullPokemon(poke)) return { value: poke, include: true };
+          const species = await P.getSpecies(poke.species.name);
+          const match = species.flavor_text_entries.find(({ version }) =>
+            versions.includes(version.name)
+          );
+          return { value: poke, include: !!match };
+        })
+      );
+      setAllPokemon(
+        queryResults.filter(({ include }) => include).map(({ value }) => value)
+      );
     };
-    fetchPokemon();
-  }, [setAllPokemon]);
+    if (versionGroup) fetchPokemon();
+  }, [setAllPokemon, versionGroup]);
 
   return (
     <div>
