2b6405ab2d0799f9a9c72240c6355e75bbba476b
effervescent000 <kislio01@gmail.com>
2023-03-22 15:33:50 -0400

    separate scoring into raw % values and scores

--- app/constants/types-constants.ts
+++ app/constants/types-constants.ts
@@ -29,10 +29,10 @@ export const OFFENSIVE_KEY = "offensive";
 export const DEFENSIVE_KEY = "defensive";
 
 export const DAMAGE_RELATION_VALUES: { [key: string]: number } = {
-  double_damage_from: -1,
-  double_damage_to: 1,
-  half_damage_from: 1,
-  half_damage_to: 0,
-  no_damage_from: 1.5,
+  double_damage_from: 2,
+  double_damage_to: 2,
+  half_damage_from: 0.5,
+  half_damage_to: 0.5,
+  no_damage_from: 0,
   no_damage_to: 0,
 };
