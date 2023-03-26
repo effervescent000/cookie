78debb2ec69e0fede98c403dcc9ed761c1fd3754
effervescent000 <kislio01@gmail.com>
2023-03-26 19:59:58 -0400

    hide some stuff on small screens

--- app/components/common/sprite-frame.tsx
+++ app/components/common/sprite-frame.tsx
@@ -1,4 +1,5 @@
 import type { IPokemonFull, IResourceListItem } from "~/interfaces";
+import useWindowSize from "~/utils/hooks/use-window-size";
 import { isFullPokemon } from "~/utils/type-guards";
 
 const SpriteFrame = ({
@@ -6,6 +7,7 @@ const SpriteFrame = ({
 }: {
   pokemon: IPokemonFull | IResourceListItem;
 }) => {
+  const { windowSize } = useWindowSize();
   if (isFullPokemon(pokemon)) {
     const sprites = [
       pokemon.sprites.front_default,
@@ -13,13 +15,8 @@ const SpriteFrame = ({
     ];
     return (
       <div className="flex h-20 w-20 justify-center xl:w-40">
-        {/* {sprites
-          .filter((sprite) => !!sprite)
-          .map((sprite) => (
-            <img key={sprite} src={sprite || undefined} alt="Sprite" />
-          ))} */}
         <img key={sprites[0]} src={sprites[0] || undefined} alt="Sprite" />
-        {sprites[1] && (
+        {sprites[1] && windowSize > 1080 && (
           <img
             className="hidden xl:block"
             key={sprites[1]}
