c06e4fcf8374047e9818a4ad30537f6aebd3bf5d
effervescent000 <kislio01@gmail.com>
2023-03-26 18:43:25 -0400

    re-break it

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,9 +1,16 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderArgs } from "@remix-run/node";
 
+import { prisma } from "~/db.server";
+
 export async function loader({ request }: LoaderArgs) {
   const host =
     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
 
-  return true;
+  try {
+    return new Response("OK");
+  } catch (error: unknown) {
+    console.log("healthcheck ❌", { error });
+    return new Response("ERROR", { status: 500 });
+  }
 }
